b1a09cf7bcd6402e4d5631becff0dd3285d991ae
Paul Seyfert <pseyfert.mathphys@gmail.com>
2018-08-31 14:12:41 +0200

    run time variable length or

--- include/range/v3/view/mask.hpp
+++ include/range/v3/view/mask.hpp
@@ -23,7 +23,9 @@
 #include <range/v3/utility/concepts.hpp> // for CONCEPT_ASSERT
 #include <range/v3/utility/functional.hpp> // for operator|, make_pipeable
 #include <range/v3/view/all.hpp> // for all
+#include <range/v3/view/any_view.hpp>
 #include <range/v3/view/filter.hpp> // for filter
+#include <range/v3/view/slice.hpp>
 #include <range/v3/view/transform.hpp> // for transform
 #include <range/v3/view/zip.hpp> // for zip
 #include <utility> // for forward
@@ -163,6 +165,146 @@ namespace ranges
                 }
             };
             RANGES_INLINE_VARIABLE(make_or_mask_fn, make_or_masker)
+
+            struct make_vector_or_mask_fn
+            {
+            private:
+                template<typename Msk1, typename Msk2>
+                static auto or_ranges(Msk1&& msk1, Msk2&& msk2)
+                {
+                    CONCEPT_ASSERT(Range<Msk1>());
+                    CONCEPT_ASSERT(Range<Msk2>());
+                    return ranges::view::zip(std::forward<Msk1>(msk1),
+                                             std::forward<Msk2>(msk2)) |
+                           ranges::view::transform(
+                               [](auto&& range_item) -> bool {
+                                   return range_item.first || range_item.second;
+                               });
+                }
+                template<typename... Msks>
+                static auto or_ranges(Msks&&... msks)
+                {
+                    CONCEPT_ASSERT((Range<Msks>() || ...));
+                    return ranges::view::zip(std::forward<Msks>(msks)...) |
+                           ranges::view::transform(
+                               [](auto&& range_item) -> bool {
+                                   return tuple_or(range_item);
+                               });
+                }
+                template<typename... T>
+                static bool variable_length_or(const T... v)
+                {
+                    return (v || ...);
+                }
+                template<typename... T, std::size_t... Idx>
+                static bool tuple_or(const std::tuple<T...> t,
+                                     std::index_sequence<Idx...>)
+                {
+                    return variable_length_or(std::get<Idx>(t)...);
+                }
+                template<typename... T>
+                static bool tuple_or(const std::tuple<T...> t)
+                {
+                    return tuple_or(t, std::index_sequence_for<T...>{});
+                }
+
+            public:
+                template<typename Msks_>
+                ranges::v3::any_view<bool> shortlist(
+                    Msks_&& msks) const // TODO const and reference and stuff
+                // precondition msks is size 1..8
+                {
+                    using Msks = decltype(msks);
+                    CONCEPT_ASSERT(ForwardRange<Msks>());
+
+                    // // todo return range with all true (an or of zero
+                    // elements
+                    // // is true)
+                    if(msks.size() == 1)
+                        return msks[0];
+                    if(msks.size() == 2)
+                        return or_ranges(msks[0], msks[1]);
+                    if(msks.size() == 3)
+                        return or_ranges(msks[0], msks[1], msks[2]);
+                    if(msks.size() == 4)
+                        return or_ranges(msks[0], msks[1], msks[2], msks[3]);
+                    if(msks.size() == 5)
+                        return or_ranges(
+                            msks[0], msks[1], msks[2], msks[3], msks[4]);
+                    if(msks.size() == 6)
+                        return or_ranges(msks[0],
+                                         msks[1],
+                                         msks[2],
+                                         msks[3],
+                                         msks[4],
+                                         msks[5]);
+                    if(msks.size() == 7)
+                        return or_ranges(msks[0],
+                                         msks[1],
+                                         msks[2],
+                                         msks[3],
+                                         msks[4],
+                                         msks[5],
+                                         msks[6]);
+                    return or_ranges(msks[0],
+                                     msks[1],
+                                     msks[2],
+                                     msks[3],
+                                     msks[4],
+                                     msks[5],
+                                     msks[6],
+                                     msks[7]);
+                }
+                template<typename Msks_>
+                ranges::v3::any_view<bool> operator()(
+                    Msks_&& msks) const // TODO const and reference and stuff
+                {
+                    using Msks = decltype(msks);
+                    CONCEPT_ASSERT(ForwardRange<Msks>());
+
+                    // // todo return range with all true (an or of zero
+                    // elements
+                    // // is true)
+                    assert(msks.size() != 0);
+                    if(msks.size() <= 8)
+                    {
+                        return shortlist(std::move(msks));
+                    }
+
+                    size_t low = ((msks.size() + 5) % 7) + 2;
+
+                    // below the long version
+                    // if(msks.size() % 7 == 0)
+                    // {
+                    //     low = 7;
+                    // }
+                    // else if(msks.size() % 7 == 1)
+                    // {
+                    //     low = 8;
+                    // }
+                    // else
+                    // {
+                    //     low = msks.size() % 7;
+                    // }
+                    ranges::v3::any_view<bool> start =
+                        shortlist(ranges::view::slice(msks, 0, low));
+                    for(; low + 6 < msks.size(); low += 7)
+                    {
+                        start = or_ranges(start,
+                                          msks[low + 0],
+                                          msks[low + 1],
+                                          msks[low + 2],
+                                          msks[low + 3],
+                                          msks[low + 4],
+                                          msks[low + 5],
+                                          msks[low + 6]);
+                    }
+                    assert(low == msks.size());
+                    return start;
+                }
+            };
+            RANGES_INLINE_VARIABLE(make_vector_or_mask_fn,
+                                   make_vector_or_masker)
 
             struct apply_or_mask_fn
             {
