551589186f53d8819885ac93bf1e9a91c7dead47
Paul Seyfert <pseyfert.mathphys@gmail.com>
2018-09-05 18:53:37 +0200

    benchmark compile time or vs run time or
    looks like for many lists, the runtime or is faster?!?

--- benchmarks/quick_bench.cpp
+++ benchmarks/quick_bench.cpp
@@ -120,6 +120,159 @@ static void use_int_backend( benchmark::State& state )
   }
 }
 
+static void fixed_length_runtime_8minus( benchmark::State& state )
+{
+  auto widgets = generate_widgets();
+
+  // std::vector<int> m0 = generate_flags<int>( 33 );
+  // std::vector<int> m1 = generate_flags<int>( 44 );
+  // std::vector<int> m2 = generate_flags<int>( 55 );
+  // std::vector<int> m3 = generate_flags<int>( 55 );
+  // std::vector<int> m4 = generate_flags<int>( 34 );
+  // std::vector<int> m5 = generate_flags<int>( 45 );
+  // std::vector<int> m6 = generate_flags<int>( 56 );
+  // std::vector<int> m7 = generate_flags<int>( 56 );
+  std::vector<std::vector<int>> varlengthmasks;
+  varlengthmasks.push_back( generate_flags<int>( 33 ) );
+  varlengthmasks.push_back( generate_flags<int>( 44 ) );
+  varlengthmasks.push_back( generate_flags<int>( 55 ) );
+  varlengthmasks.push_back( generate_flags<int>( 55 ) );
+  varlengthmasks.push_back( generate_flags<int>( 34 ) );
+  varlengthmasks.push_back( generate_flags<int>( 45 ) );
+  varlengthmasks.push_back( generate_flags<int>( 56 ) );
+  varlengthmasks.push_back( generate_flags<int>( 56 ) );
+
+  for ( auto _ : state ) {
+    auto asdf = ranges::view::make_int_vector_or_masker( varlengthmasks );
+    for ( auto& el : ranges::view::masker( widgets, asdf ) ) {
+      benchmark::DoNotOptimize( el );
+    }
+  }
+}
+
+static void fixed_length_compiletime_8minus( benchmark::State& state )
+{
+  auto widgets = generate_widgets();
+
+  std::vector<int> m0 = generate_flags<int>( 33 );
+  std::vector<int> m1 = generate_flags<int>( 44 );
+  std::vector<int> m2 = generate_flags<int>( 55 );
+  std::vector<int> m3 = generate_flags<int>( 55 );
+  std::vector<int> m4 = generate_flags<int>( 34 );
+  std::vector<int> m5 = generate_flags<int>( 45 );
+  std::vector<int> m6 = generate_flags<int>( 56 );
+  std::vector<int> m7 = generate_flags<int>( 56 );
+  // std::vector<std::vector<int>> varlengthmasks;
+  // varlengthmasks.push_back(generate_flags<int>( 33 ));
+  // varlengthmasks.push_back(generate_flags<int>( 44 ));
+  // varlengthmasks.push_back(generate_flags<int>( 55 ));
+  // varlengthmasks.push_back(generate_flags<int>( 55 ));
+  // varlengthmasks.push_back(generate_flags<int>( 34 ));
+  // varlengthmasks.push_back(generate_flags<int>( 45 ));
+  // varlengthmasks.push_back(generate_flags<int>( 56 ));
+  // varlengthmasks.push_back(generate_flags<int>( 56 ));
+
+  for ( auto _ : state ) {
+    // auto asdf = ranges::view::make_int_vector_or_masker( varlengthmasks );
+    auto asdf = ranges::view::make_or_masker( m0, m1, m2, m3, m4, m5, m6, m7 );
+    for ( auto& el : ranges::view::masker( widgets, asdf ) ) {
+      benchmark::DoNotOptimize( el );
+    }
+  }
+}
+
+static void fixed_length_runtime_8plus( benchmark::State& state )
+{
+  auto widgets = generate_widgets();
+
+  // std::vector<int> m0 = generate_flags<int>( 33 );
+  // std::vector<int> m1 = generate_flags<int>( 44 );
+  // std::vector<int> m2 = generate_flags<int>( 55 );
+  // std::vector<int> m3 = generate_flags<int>( 55 );
+  // std::vector<int> m4 = generate_flags<int>( 34 );
+  // std::vector<int> m5 = generate_flags<int>( 45 );
+  // std::vector<int> m6 = generate_flags<int>( 56 );
+  // std::vector<int> m7 = generate_flags<int>( 56 );
+  // std::vector<int> m8 = generate_flags<int>( 34 );
+  // std::vector<int> m9 = generate_flags<int>( 45 );
+  std::vector<std::vector<int>> varlengthmasks;
+  varlengthmasks.push_back( generate_flags<int>( 33 ) );
+  varlengthmasks.push_back( generate_flags<int>( 44 ) );
+  varlengthmasks.push_back( generate_flags<int>( 55 ) );
+  varlengthmasks.push_back( generate_flags<int>( 55 ) );
+  varlengthmasks.push_back( generate_flags<int>( 34 ) );
+  varlengthmasks.push_back( generate_flags<int>( 45 ) );
+  varlengthmasks.push_back( generate_flags<int>( 56 ) );
+  varlengthmasks.push_back( generate_flags<int>( 56 ) );
+  varlengthmasks.push_back( generate_flags<int>( 34 ) );
+  varlengthmasks.push_back( generate_flags<int>( 45 ) );
+
+  varlengthmasks.push_back( generate_flags<int>( 35 ) );
+  varlengthmasks.push_back( generate_flags<int>( 46 ) );
+  varlengthmasks.push_back( generate_flags<int>( 57 ) );
+  varlengthmasks.push_back( generate_flags<int>( 57 ) );
+  varlengthmasks.push_back( generate_flags<int>( 36 ) );
+  varlengthmasks.push_back( generate_flags<int>( 47 ) );
+  varlengthmasks.push_back( generate_flags<int>( 58 ) );
+  varlengthmasks.push_back( generate_flags<int>( 58 ) );
+  varlengthmasks.push_back( generate_flags<int>( 36 ) );
+  varlengthmasks.push_back( generate_flags<int>( 47 ) );
+
+  for ( auto _ : state ) {
+    auto asdf = ranges::view::make_int_vector_or_masker( varlengthmasks );
+    for ( auto& el : ranges::view::masker( widgets, asdf ) ) {
+      benchmark::DoNotOptimize( el );
+    }
+  }
+}
+
+static void fixed_length_compiletime_8plus( benchmark::State& state )
+{
+  auto widgets = generate_widgets();
+
+  std::vector<int> m0 = generate_flags<int>( 33 );
+  std::vector<int> m1 = generate_flags<int>( 44 );
+  std::vector<int> m2 = generate_flags<int>( 55 );
+  std::vector<int> m3 = generate_flags<int>( 55 );
+  std::vector<int> m4 = generate_flags<int>( 34 );
+  std::vector<int> m5 = generate_flags<int>( 45 );
+  std::vector<int> m6 = generate_flags<int>( 56 );
+  std::vector<int> m7 = generate_flags<int>( 56 );
+  std::vector<int> m8 = generate_flags<int>( 34 );
+  std::vector<int> m9 = generate_flags<int>( 45 );
+
+  std::vector<int> m10 = generate_flags<int>( 35 );
+  std::vector<int> m11 = generate_flags<int>( 46 );
+  std::vector<int> m12 = generate_flags<int>( 57 );
+  std::vector<int> m13 = generate_flags<int>( 57 );
+  std::vector<int> m14 = generate_flags<int>( 36 );
+  std::vector<int> m15 = generate_flags<int>( 47 );
+  std::vector<int> m16 = generate_flags<int>( 58 );
+  std::vector<int> m17 = generate_flags<int>( 58 );
+  std::vector<int> m18 = generate_flags<int>( 36 );
+  std::vector<int> m19 = generate_flags<int>( 47 );
+  // std::vector<std::vector<int>> varlengthmasks;
+  // varlengthmasks.push_back(generate_flags<int>( 33 ));
+  // varlengthmasks.push_back(generate_flags<int>( 44 ));
+  // varlengthmasks.push_back(generate_flags<int>( 55 ));
+  // varlengthmasks.push_back(generate_flags<int>( 55 ));
+  // varlengthmasks.push_back(generate_flags<int>( 34 ));
+  // varlengthmasks.push_back(generate_flags<int>( 45 ));
+  // varlengthmasks.push_back(generate_flags<int>( 56 ));
+  // varlengthmasks.push_back(generate_flags<int>( 56 ));
+  // varlengthmasks.push_back(generate_flags<int>( 34 ));
+  // varlengthmasks.push_back(generate_flags<int>( 45 ));
+
+  for ( auto _ : state ) {
+    // auto asdf = ranges::view::make_int_vector_or_masker( varlengthmasks );
+    auto asdf = ranges::view::make_or_masker( m0, m1, m2, m3, m4, m5, m6, m7, m8, m9,
+                                              m10, m11, m12, m13, m14, m15, m16, m17, m18, m19 );
+    for ( auto& el : ranges::view::masker( widgets, asdf ) ) {
+      benchmark::DoNotOptimize( el );
+    }
+  }
+}
+
 auto compute_min = []( const std::vector<double>& v ) -> double {
   return *( std::min_element( std::begin( v ), std::end( v ) ) );
 };
@@ -129,6 +282,11 @@ BENCHMARK( use_boolbool )   ->ComputeStatistics( "min", compute_min )/*->ThreadR
 BENCHMARK( use_int )        ->ComputeStatistics( "min", compute_min )/*->ThreadRange( 1, 4 )*/->UseRealTime();
 BENCHMARK( use_int_backend )->ComputeStatistics( "min", compute_min )/*->ThreadRange( 1, 4 )*/->UseRealTime();
 BENCHMARK( use_bool )       ->ComputeStatistics( "min", compute_min )/*->ThreadRange( 1, 4 )*/->UseRealTime();
+
+BENCHMARK(fixed_length_runtime_8minus)    ->ComputeStatistics( "min", compute_min )/*->ThreadRange( 1, 4 )*/->UseRealTime();
+BENCHMARK(fixed_length_runtime_8plus)     ->ComputeStatistics( "min", compute_min )/*->ThreadRange( 1, 4 )*/->UseRealTime();
+BENCHMARK(fixed_length_compiletime_8minus)->ComputeStatistics( "min", compute_min )/*->ThreadRange( 1, 4 )*/->UseRealTime();
+BENCHMARK(fixed_length_compiletime_8plus) ->ComputeStatistics( "min", compute_min )/*->ThreadRange( 1, 4 )*/->UseRealTime();
 // clang-format on
 
 BENCHMARK_MAIN();
